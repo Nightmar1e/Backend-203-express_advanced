afe48ba435a3ca6d654af7ddbff2685b3bd02b36
Nightmar1e <walidelghalidi2001@gmail.com>
2023-08-03 15:42:49 +0200

    Update message.js

--- Login/models/message.js
+++ Login/models/message.js
@@ -1,18 +1,12 @@
 // models/message.js
 const mongoose = require('mongoose');
 
-// const messageSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   email: { type: String, required: true },
-//   content: { type: String, required: true },
-// });
-
 const messageSchema = new mongoose.Schema({
   name: { type: String, required: true },
   content: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
-});
-// Creating message instances
+}, { collection: 'message' });
+
 
 
 
